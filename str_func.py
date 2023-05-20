f3a290c32244fb9b6a6b9a7b13be4374f152876c
Балахонцева Анастасия <ayamileto@gmail.com>
2023-05-20 03:12:40 +0300

    add second func

--- str_func.py
+++ str_func.py
@@ -1,3 +1,13 @@
 def upper_str(string):
     upper_string = string.upper()
     return upper_string
+
+
+def up_first_letter(string):
+    """ Функция делает заглавными первые буквы каждого слова в строке """
+    str_list = string.split(" ")
+    upper_letters_str = []
+    for word in str_list:
+        up_letter_word = word.capitalize()
+        upper_letters_str.append(up_letter_word)
+    return ' '.join(upper_letters_str)
